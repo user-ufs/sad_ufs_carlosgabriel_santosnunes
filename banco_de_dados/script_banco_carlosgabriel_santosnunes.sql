265bf5f5ee64073437266b454f6217a3ed5a32e5
gabrieels <gabrieelsantosn@gmail.com>
2016-08-09 17:47:15 -0300

    Inserindo arquivos da ativade 4.

---
+++ banco_de_dados/script_banco_carlosgabriel_santosnunes.sql
@@ -0,0 +1,14 @@
+-- Database: postgres
+
+-- DROP DATABASE postgres;
+
+CREATE DATABASE postgres
+  WITH OWNER = postgres
+       ENCODING = 'UTF8'
+       TABLESPACE = pg_default
+       LC_COLLATE = 'Portuguese_Brazil.1252'
+       LC_CTYPE = 'Portuguese_Brazil.1252'
+       CONNECTION LIMIT = -1;
+
+COMMENT ON DATABASE postgres
+  IS 'default administrative connection database';
